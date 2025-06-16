b1ba0446db4d988e6eee1cb4845d86a6e079bb33
GuptaShubham-11 <shu919847@gmail.com>
2025-06-16 17:45:17 +0530

    add loader

---
+++ client/src/components/Spinner.tsx
@@ -0,0 +1,12 @@
+import { LoaderCircle } from 'lucide-react';
+
+export default function Spinner({ size = 20 }: { size?: number }) {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <LoaderCircle
+                className="animate-[spin_0.5s_linear_infinite]"
+                size={size}
+            />
+        </div>
+    );
+}
