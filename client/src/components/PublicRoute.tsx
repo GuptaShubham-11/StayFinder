6f57600d78d0693e9cf7cb02b9cdf985d2b13117
GuptaShubham-11 <shu919847@gmail.com>
2025-06-17 12:40:20 +0530

    add PublicRoute component for user authentication handling

---
+++ client/src/components/PublicRoute.tsx
@@ -0,0 +1,9 @@
+import { Navigate } from "react-router-dom";
+import { useAuthStore } from "../store/authStore";
+import type { ReactNode } from "react";
+
+
+export default function PublicRoute({ children }: { children: ReactNode }) {
+    const { user } = useAuthStore();
+    return !user ? <>{children}</> : <Navigate to="/listings" replace />;
+}
