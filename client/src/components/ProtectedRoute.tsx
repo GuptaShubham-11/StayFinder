5e41024d53e33f52339a332c38392dad6d3f3e9d
GuptaShubham-11 <shu919847@gmail.com>
2025-06-17 12:40:35 +0530

    add ProtectedRoute component for user authentication

---
+++ client/src/components/ProtectedRoute.tsx
@@ -0,0 +1,9 @@
+import { useAuthStore } from "@/store/authStore";
+import type { ReactNode } from "react";
+import { Navigate } from "react-router-dom";
+
+
+export default function ProtectedRoute({ children }: { children: ReactNode }) {
+    const { user } = useAuthStore();
+    return user ? <>{children}</> : <Navigate to="/signin" replace />;
+}
