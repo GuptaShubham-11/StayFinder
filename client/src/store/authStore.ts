db69a98496b3c415e19fa999077d9853c32c2e4b
GuptaShubham-11 <shu919847@gmail.com>
2025-06-17 12:42:44 +0530

    add authStore to use User interface and update state management

--- client/src/store/authStore.ts
+++ client/src/store/authStore.ts
@@ -1,11 +1,19 @@
-import { create } from 'zustand';
+import { create } from 'zustand'
 
+interface User {
+    id: string
+    email: string
+    role: string
+}
 
+interface AuthState {
+    user: User | null;
+    login: (user: any) => void;
+    logout: () => void;
+}
 
-const authStore = create((set) => ({
-    userProfile: null,
-    addUser: (user: any) => set({ userProfile: user }),
-    removeUser: () => set({ userProfile: null }),
+export const useAuthStore = create<AuthState>((set) => ({
+    user: null,
+    login: (user: User) => set({ user }),
+    logout: () => set({ user: null }),
 }));
-
-export default authStore;
